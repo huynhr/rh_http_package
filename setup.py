45748a4c28b26208f87cbf47c5f48a779bb2cfc4
Raymond Huynh <huynh.r.k@gmail.com>
2019-03-30 17:37:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='rh_http_package',
+setup(name='rhhttppackage',
       version='0.1',
       description='Chuck norris jokes',
       url='https://github.com/huynhr/rh_http_package',
