663611ab781ea5c4e95bbf9730a94ef45296bf7c
rhmerx <huynh.r.k@gmail.com>
2019-03-30 17:57:40 -0400

    updated name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='rhhttppackage',
+setup(name='rh_http_package',
       version='0.1',
       description='Chuck norris jokes',
       url='https://github.com/huynhr/rh_http_package',
